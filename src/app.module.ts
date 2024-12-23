4e3f326cbea8c1b6b4cadb6e073f1711695c9f0f
vivek vala <vivek.citrusbug@gmail.com>
2024-12-23 18:46:46 +0530

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  MiddlewareConsumer,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
 import { DatabaseModule } from './database/database.module';
 import { CommonModule } from './common/common.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { EmployeeController } from './employee/employee.controller';
+
 @Module({
   imports: [EmployeeModule, DatabaseModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('employee');    option 1
+
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({ path: 'employee', method: RequestMethod.POST });  option 2
+
+    consumer.apply(LoggerMiddleware).forRoutes(EmployeeController);
+  }
+}
---
+++ src/songs/dto/create-song.dto.ts
@@ -0,0 +1,40 @@
+import {
+  IsString,
+  IsNotEmpty,
+  MaxLength,
+  IsArray,
+  ArrayNotEmpty,
+  IsDate,
+  IsISO8601,
+  ArrayMaxSize,
+  IsPositive,
+  Min,
+  Max,
+  Validate,
+  IsDateString,
+  IsMilitaryTime,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateSong {
+  @IsString()
+  @IsNotEmpty({ message: 'Title cannot be empty' })
+  @MaxLength(100, { message: 'Title must not exceed 100 characters' })
+  title: string;
+
+  @IsArray({ message: 'Artists must be an array of strings' })
+  @ArrayNotEmpty({ message: 'Artists array cannot be empty' })
+  @MaxLength(50, {
+    each: true,
+    message: 'Each artist name must not exceed 50 characters',
+  })
+  artists: string[];
+
+  @IsNotEmpty({ message: 'Release date cannot be empty' })
+  @IsDateString()
+  releaseDate: Date;   //2020-09-28
+
+  @IsMilitaryTime()
+  @IsNotEmpty({ message: 'duration cannot be empty' }) 
+  duration: Date;  //02:30
+}
